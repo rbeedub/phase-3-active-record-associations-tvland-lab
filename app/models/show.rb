1c85516dd84e23b252a6707706ee800ff15e0341
Rebecca Wollard <rbwollard@gmail.com>
2023-02-06 16:30:28 -0700

    commit tree 793fe380d71f91de70512fa792cdb71924714df9

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    self.characters.map {|c| "#{c.actor.first_name} #{c.actor.last_name}"}
+  end 
 end
